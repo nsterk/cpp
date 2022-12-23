0ecc4b909f037797cf930078a997c46526e3aeba
Naomi Sterk <nsterk@student.codam.nl>
2022-12-23 13:23:03 +0100

    [ADD] tests for 01.00

--- mod01/ex00/srcs/main.cpp
+++ mod01/ex00/srcs/main.cpp
@@ -6,22 +6,28 @@
 /*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/03 15:08:45 by nsterk        #+#    #+#                 */
-/*   Updated: 2022/11/07 19:11:35 by nsterk        ########   odam.nl         */
+/*   Updated: 2022/12/23 13:22:20 by nsterk        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Zombie.hpp>
+#include <iostream>
 
 Zombie *newZombie (std::string name);
 void	randomChump(std::string name);
 
 int	main(void)
 {
-	Zombie Momo = Zombie("Momo");
-	Zombie *Owow = newZombie("Owow");
+	std::cout << "\033[35mZombie A = Zombie(\"A\");\033[0m" << std::endl;	
+	Zombie A = Zombie("A");
+	std::cout << "\033[35mZombie *B = newZombie(\"B\");\033[0m" << std::endl;
+	Zombie *B = newZombie("B");
 
+	std::cout << "\033[35mrandomChump(\"random dude\");\033[0m" << std::endl;
 	randomChump("random dude");
-	Momo.announce();
-	delete Owow;
+	std::cout << "\033[35mA.announce():\033[0m" << std::endl;
+	A.announce();
+	B->announce();
+	delete B;
 	return (0);
 }
