4777258706aa33fbea1ea915a2041fadceac30ea
Naomi Sterk <nsterk@student.codam.nl>
2022-12-27 14:20:06 +0100

    [SYNC] testing

---
+++ mod01/ex00/inc/colours.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   colours.hpp                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/12/23 17:55:53 by nsterk        #+#    #+#                 */
+/*   Updated: 2022/12/23 22:21:32 by nsterk        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOURS_HPP
+# define COLOURS_HPP
+
+#define CYAN "\033[36m"
+#define MAGENTA "\033[35m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+#endif
