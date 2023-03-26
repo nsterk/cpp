6142b3eb5d2646a7a79bdbdac86a41bd2359b319
Naomi Sterk <nsterk@student.codam.nl>
2023-03-26 23:58:03 +0200

    clean up

--- mod02/ex02/srcs/main.cpp
+++ mod02/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/29 20:57:58 by nsterk        #+#    #+#                 */
-/*   Updated: 2023/03/26 23:19:03 by nsterk        ########   odam.nl         */
+/*   Updated: 2023/03/26 23:34:59 by nsterk        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,15 @@ void multiDiviTests(Fixed const A, Fixed const B);
 void incrementDecrementTests(void);
 
 int	main(void) {
-	Fixed		A(8.1f);
-	Fixed		B(0.1f);
-	Fixed const C(8);
-	Fixed const E(Fixed(5.05f) * Fixed(2));
-	Fixed const F(Fixed(5.05f) / Fixed(2));
 
-	// subtractionTests(Fixed(-10), Fixed(2.5f));
-	// comparisonTests(Fixed(1), Fixed(1.005f));
-	// multiDiviTests(Fixed(5.05f), Fixed(2));
-	// incrementDecrementTests();
-	std::cout << Fixed::min(F, A) << std::endl;
-	std::cout << Fixed::max(F, A) << std::endl;
+	Fixed const A(8);
+
+	subtractionTests(Fixed(-10), Fixed(2.5f));
+	comparisonTests(Fixed(1), Fixed(1.005f));
+	multiDiviTests(Fixed(5.05f), Fixed(2));
+	incrementDecrementTests();
+
+	std::cout << Fixed::min(A, Fixed (8.1f)) << std::endl;
+	std::cout << Fixed::max(A, Fixed (8.1f)) << std::endl;
 	return (0);
 }
--- mod02/ex02/srcs/tests.cpp
+++ mod02/ex02/srcs/tests.cpp
@@ -6,13 +6,18 @@
 /*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/20 16:59:33 by nsterk        #+#    #+#                 */
-/*   Updated: 2023/03/26 23:18:58 by nsterk        ########   odam.nl         */
+/*   Updated: 2023/03/26 23:36:42 by nsterk        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Fixed.hpp"
 #include "../inc/colours.hpp"
 
+/**
+ * @brief Demonstrates the + and - operators
+ * @param A Instance of Fixed point class
+ * @param B Instance of Fixed point class
+ */
 void	subtractionTests(Fixed const A, Fixed const B) {
 
 	std::cout << U_WHT"Subtraction and addition operators"RESET << std::endl;
@@ -23,6 +28,11 @@ void	subtractionTests(Fixed const A, Fixed const B) {
 	std::cout << "B - B - B = " << B - B - B << std::endl;
 }
 
+/**
+ * @brief Demonstrates the comparison operators: < > <= >= != ==
+ * @param A Instance of Fixed point class
+ * @param B Instance of Fixed point class
+ */
 void	comparisonTests(Fixed const A, Fixed const B) {
 
 	std::cout << U_WHT"Comparison operators"RESET << std::endl;
@@ -36,6 +46,11 @@ void	comparisonTests(Fixed const A, Fixed const B) {
 	std::cout << "A != B: " << (A != B) << std::endl;
 }
 
+/**
+ * @brief Demonstrates the * and / operators
+ * @param A Instance of Fixed point class
+ * @param B Instance of Fixed point class
+ */
 void	multiDiviTests(Fixed const A, Fixed const B) {
 
 	std::cout << U_WHT"Multiplication and division operators"RESET << std::endl;
@@ -45,6 +60,11 @@ void	multiDiviTests(Fixed const A, Fixed const B) {
 	std::cout << "B / A: " << B / A << std::endl;
 }
 
+/**
+ * @brief Demonstrates the increment and decrement operators
+ * @param A Instance of Fixed point class
+ * @param B Instance of Fixed point class
+ */
 void	incrementDecrementTests(void) {
 
 	Fixed a;
